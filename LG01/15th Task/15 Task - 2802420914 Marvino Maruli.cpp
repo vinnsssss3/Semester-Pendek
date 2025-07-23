30f37d51247ee1a6364f30fe8412b4f6cdd8e293
vinnsssss3 <marvinosssss3@gmail.com>
2025-07-23 19:29:52 +0700

    Add files via upload

---
+++ LG01/15th Task/15 Task - 2802420914 Marvino Maruli.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int tinggi;
+
+    scanf("%d", &tinggi);
+
+    for (int i = 1; i <= tinggi; i++) {
+        for (int spasi = 1; spasi <= tinggi - i; spasi++) {
+            printf("  ");
+        }
+        for (int bintang = 1; bintang <= (2 * i - 1); bintang++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
